0dc63348a4889cb83c481b1a15d829efe2d9b0b4
Cang <cang@lastvoices.com>
2015-08-31 15:50:52 +0700

    BIN-2666 #comment cleaning up

--- local/binumi/library/binumi_client.class.php
+++ local/binumi/library/binumi_client.class.php
@@ -438,26 +438,8 @@ class binumi_client
 	 * @return array
 	 */
 	public function get_texteditor_params() {
-		global $COURSE, $CFG;
+		global $COURSE;
 
-		//default non-lti urls
-		/* $chooser_js_url = $this->get_chooser_js_url();
-		 $chooser_url = $this->get_unsigned_chooser_url();
-		 $launch_url = null;
-
-		 if ($this->has_lti_config() && isset($COURSE->id)) {
-			 $chooser_js_url = $this->get_chooser_js_url($COURSE->id);
-			 // append the context_id to the chooser endpoint
-			 $chooser_url .= (strpos($chooser_url, '?') === false) ? '?' : '&';
-			 $chooser_url .= 'context_id=' . $COURSE->id;
-			 $site_url = $this->get_siteurl();
-			 $content_url = $CFG->wwwroot.'/local/mediacore/sign.php';
-			 $launch_url = str_replace($site_url, $content_url, $chooser_url);
-		 }
-		 $params['binumi_chooser_js_url'] = $chooser_js_url;
-		 $params['binumi_chooser_url'] = $chooser_url;
-		 $params['mcore_launch_url'] = $launch_url;
- */
 		$params['binumi_chooser_url'] = $this->get_signed_chooser_url($COURSE->id, $this->get_lti_params($COURSE));
 		return $params;
 	}
