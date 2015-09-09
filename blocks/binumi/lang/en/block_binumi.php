eb7acb753e5280a2a71816f3095e28baa187e4e6
Thanayu Thanakitworawat <dead1body@hotmail.com>
2015-09-09 09:07:09 +0700

    BIN-2735 #comment Fix dependency version.

--- blocks/binumi/lang/en/block_binumi.php
+++ blocks/binumi/lang/en/block_binumi.php
@@ -1,11 +1,20 @@
 <?php
 
 /**
- * BINUMI BLOCK FOR MOODLE
+ * __________.__                     .__
+ * \______   \__| ____  __ __  _____ |__|
+ *  |    |  _/  |/    \|  |  \/     \|  |
+ *  |    |   \  |   |  \  |  /  Y Y  \  |
+ *  |______  /__|___|  /____/|__|_|  /__|
+ *         \/        \/            \/
  *
- * @package    block_binumi
+ * Binumi's block plugin
+ *
+ * @package    block
+ * @subpackage binumi
  * @copyright  2011 - 2015 Binumi Agency Hong Kong Limited.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
  */
 
 $string['pluginname'] = 'Binumi';
--- blocks/binumi/version.php
+++ blocks/binumi/version.php
@@ -1,15 +1,26 @@
 <?php
+
 /**
- * BINUMI BLOCK FOR MOODLE
+ * __________.__                     .__
+ * \______   \__| ____  __ __  _____ |__|
+ *  |    |  _/  |/    \|  |  \/     \|  |
+ *  |    |   \  |   |  \  |  /  Y Y  \  |
+ *  |______  /__|___|  /____/|__|_|  /__|
+ *         \/        \/            \/
  *
- * @package    block_binumi
+ * Binumi's block plugin
+ *
+ * @package    block
+ * @subpackage binumi
  * @copyright  2011 - 2015 Binumi Agency Hong Kong Limited.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ *
  */
+
 $plugin                     = new StdClass();
 $plugin->component          = 'block_binumi';
 $plugin->version 			= 2015090800;
-$plugin->requires 			= 2012062500;
+$plugin->requires 			= 2014041100;
 $plugin->release  			= '1.0.0';
 $plugin->maturity 			= MATURITY_STABLE;
 $plugin->dependencies       = array(
