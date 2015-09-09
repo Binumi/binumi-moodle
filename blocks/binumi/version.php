4c35f01a491fbac5e65ae3e65c21e01fdc141bc0
Thanayu Thanakitworawat <dead1body@hotmail.com>
2015-09-09 08:52:34 +0700

    BIN-2735 #comment Fix version.php

--- blocks/binumi/version.php
+++ blocks/binumi/version.php
@@ -6,10 +6,12 @@
  * @copyright  2011 - 2015 Binumi Agency Hong Kong Limited.
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-$plugin->version = 2015090800;
-$plugin->requires = 2012062500;
-$plugin->release  = '1.0.0';
-$plugin->maturity = MATURITY_STABLE;
+$plugin                     = new StdClass();
+$plugin->component          = 'block_binumi';
+$plugin->version 			= 2015090800;
+$plugin->requires 			= 2012062500;
+$plugin->release  			= '1.0.0';
+$plugin->maturity 			= MATURITY_STABLE;
 $plugin->dependencies       = array(
 	'local_binumi' => 2015090800,
 );
